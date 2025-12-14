1c0a0e54c0615b292194e19863d59e5da73e81ba
Houcine Abdaoui <hcnabdaoui@gmail.com>
2025-12-14 10:49:45 +0100

    -

--- exercice-2.py
+++ exercice-2.py
@@ -1,31 +1,24 @@
 # Every time you add a new payment method, you have to modify the `PaymentProcessor` class to add another `elif` branch. This violates the Open/Closed Principle (OCP) because the class is not closed for modification—changes require editing existing code, which can introduce bugs.
 
 # ------------------------------------------------------------
-
-from abc import ABC, abstractmethod
-
-class PaymentMethod(ABC):
-    @abstractmethod
-    def pay(self, amount):
-        pass
-
-class CreditCardPayment(PaymentMethod):
+class CreditCardPayment:
     def pay(self, amount):
         print(f"Paid {amount} with credit card")
 
-class PayPalPayment(PaymentMethod):
+class PayPalPayment:
     def pay(self, amount):
         print(f"Paid {amount} with PayPal")
 
-class BitcoinPayment(PaymentMethod):
+class BitcoinPayment:
     def pay(self, amount):
         print(f"Paid {amount} with Bitcoin")
 
 class PaymentProcessor:
-    def process_payment(self, amount, method: PaymentMethod):
+    def process_payment(self, amount, method):
         method.pay(amount)
 
 processor = PaymentProcessor()
+
 processor.process_payment(100, CreditCardPayment())
 processor.process_payment(50, PayPalPayment())
 processor.process_payment(0.01, BitcoinPayment())
